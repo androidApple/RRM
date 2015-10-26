69dd90218fc08ea980c4032c86db7881c8a28ab6
androidApple <androidappledev2015@gmail.com>
2015-10-26 11:54:26 +0600

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -10,21 +10,8 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','n
 
 
 
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleLightContent();
-    }
-  });
+.run(function($ionicPlatform,  $ionicPopup/*, $cordovaDevice*/) {
+ 
 })
 
 .config(function($stateProvider, $urlRouterProvider) {
@@ -274,5 +261,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','n
 	
   // if none of the above states are matched, use this as the fallback
  $urlRouterProvider.otherwise("/tab/signin");
+  $ionicConfigProvider.tabs.position('bottom');
 
 });
